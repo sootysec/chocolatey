6abdb11e8c483427357c6b3282991f4e507ac44c
Geralt18 <geralt18@gmail.com>
2021-05-27 14:25:30 +0200

    Updated to win-acme 2.1.17.1065

--- win-acme/tools/chocolateyinstall.ps1
+++ win-acme/tools/chocolateyinstall.ps1
@@ -3,12 +3,12 @@ $packageArgs = @{
 
   UnzipLocation          = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
   
-  Url                    = 'https://github.com/win-acme/win-acme/releases/download/v2.1.12/win-acme.v2.1.12.943.x86.pluggable.zip'
-  Checksum               = 'A890DB34B78562F37D4D482EC3121890DC2FF18C91DF79A2FA3BC1F258CEA753'
+  Url                    = 'https://github.com/win-acme/win-acme/releases/download/v2.1.17.1065/win-acme.v2.1.17.1065.x86.pluggable.zip'
+  Checksum               = '14283777906F20A55C630DABA8EF58FC134676B27611197A7C6D6FF1E15FAB5F'
   ChecksumType           = 'sha256'
 
-  Url64bit               = 'https://github.com/win-acme/win-acme/releases/download/v2.1.12/win-acme.v2.1.12.943.x64.pluggable.zip'
-  Checksum64             = 'C761F708EF5ADE7C988AA8093181682F21206F2A1655317A5B8C12D06FC49D4D'
+  Url64bit               = 'https://github.com/win-acme/win-acme/releases/download/v2.1.17.1065/win-acme.v2.1.17.1065.x64.pluggable.zip'
+  Checksum64             = '406C4E06184B176F9BC43BECD386B9D0958C2933AF144DD64650E2D5F22DE49C'
   ChecksumType64         = 'sha256'
 }
 
